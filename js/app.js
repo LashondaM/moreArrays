01edcd6453ed25a1b2a9c35db2e590f2fd0e8406
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-15 01:43:03 -0400

    more Arrays

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var oreo = ['Billy', 2, 'Mandy', 2, 'Mac', 3, 'Cheese', 4, 'Liza', 'Koshy'];
+    console.log(oreo);
+
+    // function butter(arr, lemon) {
+    //     let arr = oreo;
+    //     while (lemon >= 1) {
+    //         arr.push(['Ray', 'Cyrus']);
+    //     }
+    //     return arr;
+    // }
+
+    // console.log(arr); ------------------------------------------
+
+
+//     function newOreo(arg1, arg2) {
+//         arg1 = oreo.push(['Ray', 5]);
+//         arg2 = oreo.push('Rick');
+//         return arg1, arg2;
+//     }
+// console.log(oreo);
+
+// SLICE METHOD
+
+function newOreo(arr) {
+    return arr = oreo.slice(2, 4);
+};
+console.log(oreo);
